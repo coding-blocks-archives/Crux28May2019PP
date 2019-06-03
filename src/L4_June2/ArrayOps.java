9106208278bd1748ee825489afb97dcf43139f78
Garima Chhikara <garimachhikara128@gmail.com>
2019-06-03 19:29:12 +0530

    Lec 5 : Arrays

--- src/L4_June2/ArrayOps.java
+++ src/L4_June2/ArrayOps.java
@@ -16,13 +16,24 @@ public class ArrayOps {
 	public static void main(String[] args) {
 
 		// int[] array = takeInput();
-		int[] array = { 10, 20, 30, 40, 50 };
+		int[] array = { 10, 20, 30, 40 };
 
-		display(array);
+		// display(array);
 
-		System.out.println(max(array));
-		System.out.println(linearSearch(array, 30));
-		System.out.println(binarySearch(array, 30));
+		// System.out.println(max(array));
+		// System.out.println(linearSearch(array, 30));
+		// System.out.println(binarySearch(array, 30));
+
+		// reverse(array);
+		// int[] ans = inverse(array);
+		// int[] ans = rotate(array, 4);
+
+		// bubbleSort(array);
+		// selectionSort(array);
+		// insertionSort(array);
+		subArrays(array);
+
+//		display(array);
 	}
 
 	public static int[] takeInput() {
@@ -97,6 +108,144 @@ public class ArrayOps {
 		}
 
 		return -1;
+
+	}
+
+	public static void reverse(int[] arr) {
+
+		int i = 0;
+		int j = arr.length - 1;
+
+		while (i < j) {
+
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+
+			i++;
+			j--;
+
+		}
+
+	}
+
+	public static int[] inverse(int[] arr) {
+
+		int[] inv = new int[arr.length];
+
+		for (int i = 0; i < inv.length; i++) {
+			inv[arr[i]] = i;
+		}
+
+		return inv;
+
+	}
+
+	public static int[] rotate(int[] arr, int r) {
+
+		r = r % arr.length;
+
+		if (r < 0) {
+			r = r + arr.length;
+		}
+
+		int[] ans = new int[arr.length];
+
+		for (int i = 0; i < ans.length; i++) {
+
+			if (i <= r - 1) {
+				ans[i] = arr[i + arr.length - r];
+			} else {
+				ans[i] = arr[i - r];
+			}
+
+		}
+
+		return ans;
+
+	}
+
+	public static void bubbleSort(int[] arr) {
+
+		int n = arr.length;
+
+		for (int counter = 0; counter <= n - 2; counter++) {
+
+			for (int j = 0; j <= n - counter - 2; j++) {
+
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+
+			}
+
+		}
+
+	}
+
+	public static void selectionSort(int[] arr) {
+
+		int n = arr.length;
+
+		for (int counter = 0; counter <= n - 2; counter++) {
+
+			int min = counter;
+
+			// find min in rest of the array
+			for (int j = counter + 1; j <= arr.length - 1; j++) {
+
+				if (arr[j] < arr[min]) {
+					min = j;
+				}
+			}
+
+			// swap
+			int temp = arr[min];
+			arr[min] = arr[counter];
+			arr[counter] = temp;
+
+		}
+
+	}
+
+	public static void insertionSort(int[] arr) {
+
+		for (int counter = 1; counter < arr.length; counter++) {
+
+			int temp = arr[counter];
+
+			int j = counter - 1;
+
+			while (j >= 0 && arr[j] >= temp) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+
+			arr[j + 1] = temp;
+
+		}
+
+	}
+
+	public static void subArrays(int[] arr) {
+
+		for (int si = 0; si <= arr.length - 1; si++) {
+
+			for (int ei = si; ei <= arr.length - 1; ei++) {
+
+				for (int k = si; k <= ei; k++) {
+					System.out.print(arr[k] + " ");
+				}
+
+				System.out.println();
+			}
+		}
+
+	}
+
+	public static void subsets(int[] arr) {
 
 	}
 
